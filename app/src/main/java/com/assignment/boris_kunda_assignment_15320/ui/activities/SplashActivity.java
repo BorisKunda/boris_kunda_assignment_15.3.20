bbe2413086313c0bbf91c99952ddde09fc833d23
BorisKunda <bskunda1992@gmail.com>
2020-03-21 19:04:43 +0200

    splash finished

--- app/src/main/java/com/assignment/boris_kunda_assignment_15320/ui/activities/SplashActivity.java
+++ app/src/main/java/com/assignment/boris_kunda_assignment_15320/ui/activities/SplashActivity.java
@@ -2,8 +2,10 @@ package com.assignment.boris_kunda_assignment_15320.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.os.CountDownTimer;
+import android.util.Log;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -12,39 +14,83 @@ import com.assignment.boris_kunda_assignment_15320.viewmodel.MovieViewModel;
 
 public class SplashActivity extends AppCompatActivity {
 
-    // TODO: 3/16/2020  add method to utils which setViewModel
-
-    // TODO: 3/17/2020 finish splash screen
-
-    private Handler mHandler;
-    private Intent mMainActivityIntent;
-    private final int SPLASH_TIME = 666000;
+    private final int SPLASH_COUNT_FINISH = 5000;
+    private final int SPLASH_COUNT_INTERVAL = 1000;
+    private boolean mIsLoadFinished = false;
     private MovieViewModel mMovieViewModel;
+    private int mTemp;
+    private AlertDialog mAlertDialog;
+    private CountDownTimer mCountDownTimer;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        mMovieViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication())).get(MovieViewModel.class);
+        setViewModel();
+        subscribeToViewModel();
+        buildDialog();
+        setCountDown();
+        mCountDownTimer.start();
 
+    }
+
+    private void setViewModel () {
+        mMovieViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication())).get(MovieViewModel.class);
+    }
+
+    private void subscribeToViewModel () {
         mMovieViewModel.getMovieListLD().observe(this, iMovies -> {
             if (iMovies.size() == 0) {
-
                 mMovieViewModel.loadMoviesList();
             } else {
-
+                mIsLoadFinished = true;
             }
         });
+    }
 
-        mHandler = new Handler();
+    private void setCountDown () {
+        mCountDownTimer = new CountDownTimer(SPLASH_COUNT_FINISH, SPLASH_COUNT_INTERVAL) {
 
-        mHandler.postDelayed(() -> {
-            mMainActivityIntent = new Intent(SplashActivity.this, MainActivity.class);
+            public void onFinish () {
+                runOnUiThread(() -> {
+                    mAlertDialog.show();
+                    cancel();
+                });
+            }
+
+            public void onTick (long millisUntilFinished) {
+                mTemp++;
+
+                if (mIsLoadFinished && mTemp > 3) {
+                    runOnUiThread(() -> {
+                        Intent mMainActivityIntent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(mMainActivityIntent);
+                        cancel();
+                        finish();
+                    });
+                }
+
+            }
+        };
+    }
+
+    private void buildDialog () {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SplashActivity.this);
+        // Add the buttons
+        builder.setPositiveButton("retry", (dialog, id) -> {
+            mMovieViewModel.loadMoviesList();
+            mCountDownTimer.start();
+        });
+        builder.setNegativeButton("close app", (dialog, id) -> {
             finish();
-            startActivity(mMainActivityIntent);
-        }, SPLASH_TIME);
+            Log.e(getClass().getSimpleName(), "onClick");
+        });
 
+        mAlertDialog = builder.create();
+        mAlertDialog.setIcon(R.drawable.error);
+        mAlertDialog.setTitle("Error");
+        mAlertDialog.setMessage("Something wrong please try again");
     }
 
 
